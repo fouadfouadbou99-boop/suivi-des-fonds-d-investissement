707deb5bc27c5f4cb40cd17a8352b97fb0496346
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-29 22:13:43 +0100

    Add vote and risk extraction functions
    Added functions to extract votes and risks from text.

--- governance.py
+++ governance.py
@@ -20,3 +20,46 @@ def extract_participants(text):
             participants.append(p)
 
     return participants
+def extract_votes(text):
+
+    votes = []
+
+    texte = text.lower()
+
+    keywords = [
+        "approuvé",
+        "rejeté",
+        "abstention",
+        "vote"
+    ]
+
+    for mot in keywords:
+
+        if mot in texte:
+
+            votes.append(mot)
+
+    return votes
+    def extract_risks(text):
+
+    risques = []
+
+    mots = [
+        "risque",
+        "litige",
+        "contentieux",
+        "défaut",
+        "retard",
+        "impayé",
+        "perte"
+    ]
+
+    texte = text.lower()
+
+    for mot in mots:
+
+        if mot in texte:
+
+            risques.append(mot)
+
+    return list(set(risques))
