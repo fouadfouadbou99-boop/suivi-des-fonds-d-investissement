aeeaad766208622b8563f961fc753eee52e7877d
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-29 21:59:21 +0100

    Implement extract_general_info function

--- general.py
+++ general.py
@@ -1,2 +1,26 @@
+import re
+
+
 def extract_general_info(text):
-    return {}
+
+    result = {
+        "nom_projet": None,
+        "date_reunion": None
+    }
+
+    if "kiara" in text.lower():
+
+        result["nom_projet"] = "Projet Kiara"
+
+    date_match = re.search(
+        r"\d{2}/\d{2}/\d{4}",
+        text
+    )
+
+    if date_match:
+
+        result["date_reunion"] = (
+            date_match.group()
+        )
+
+    return result
